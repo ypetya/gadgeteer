7fa5919b3baf7e74ff21c2062fd7acc2cd070e73
Kiss Péter <ypetya@gmail.com>
2010-01-07 18:45:14 +0100

    sometimes can not log non string parameters at link behaviour.

--- javascripts/jquery.gadgeteer.js
+++ javascripts/jquery.gadgeteer.js
@@ -604,7 +604,7 @@ $.extend($.gadgeteer, {
             if ($.isFunction(callback) && (match = callback.call(link, e))) {
                 var params = match === true ? [] : ($.isFunction(match.push) ? match : Array(match));
                 params.push(e);
-                $.gadgeteer.log('calling ' + behaviour + ' link behaviour for ' + link + ' with ' + params);
+                $.gadgeteer.log('calling ' + behaviour );
                 var handler = behaviour+'Request';
                 handler = $.gadgeteer.linkBehaviours.handlers && $.gadgeteer.linkBehaviours.handlers[handler] || $.gadgeteer[handler];
                 handler.apply(link, params);
